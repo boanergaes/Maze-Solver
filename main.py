b44ced6b989723f21dccd94cc6237f8c0ad4a7fd
Zeamanuel Mebit <mebitzeamanuel@gmail.com>
2026-05-08 02:13:32 +0300

    feat: provide intry point for the program ( main() )

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+from maze import Maze
+from constants import ROWS,  COLS, FPS, WIDTH, HEIGHT
+
+def main():
+    pygame.init()
+    
+    screen = pygame.display.set_mode((WIDTH * 1.5, HEIGHT * 1.5))
+    
+    pygame.display.set_caption("Maze Solver")
+    
+    clock = pygame.time.Clock()
+    
+    maze = Maze(ROWS, COLS)
+    
+    # generate maze
+    
+    maze.generate(screen)
+    
+    pygame.time.delay(1000)
+    
+    # solve maze
+    maze.solve(screen)
+    
+    # keep window open
+    while True:
+        maze.handle_events()
+        
+        maze.draw(screen)
+        
+        clock.tick(FPS)
+        
+        
+if __name__ == "__main__":
+    main()
