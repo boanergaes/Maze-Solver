dac8356b89703154d15718f30071485eabae9493
Zeamanuel Mebit <mebitzeamanuel@gmail.com>
2026-05-08 01:46:08 +0300

    feat: implement the Maze class
    - define the neccessary feilds
    
    - implement the helper functions

---
+++ main.py
@@ -0,0 +1,124 @@
+import pygame
+import random
+import sys
+from constants import CELL_SIZE, WHITE, BLACK, RED, BLUE, GREEN, WIDTH, HEIGHT
+
+class Maze:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        
+        self.north_wall = [
+            # [True]*cols for _ in range rows
+            [True for _ in range(cols)] for _ in range(rows)
+        ]
+        
+        self.east_wall = [
+            [True for _ in range(cols)] for _ in range(rows)
+        ]
+        
+        self.visited = [
+            [False for _ in range(cols)] for _ in range(rows)
+        ]
+        
+        self.solution_visited = [
+            [False for _ in range(cols)] for _ in range(rows)
+        ]
+        
+        self.dead_ends = set()
+        
+        self.solution_path = []
+        
+        self.start = (0, 0)
+        self.end = (rows - 1, cols - 1)
+        
+    # helpers
+    
+    def get_unvisited_neighbors(self, r, c):
+        neighbors = []
+        
+        # up
+        if r > 0 and not self.visited[r - 1][c]:
+            neighbors.append((r - 1, c))
+            
+        # down
+        if r < self.rows - 1 and not self.visited[r + 1][c]:
+            neighbors.append((r + 1, c))
+        
+        # left
+        if c > 0 and not self.visited[r][c - 1]:
+            neighbors.append((r, c - 1))
+            
+        # right
+        if c < self.cols - 1 and not self.visited[r][c + 1]:
+            neighbors.append((r, c + 1))
+        
+        return neighbors
+    
+    def remove_wall(self, r, c, nr, nc):
+        # right
+        if nr == r and nc == c + 1:
+            self.east_wall[r][c] = False
+            
+        # left 
+        elif nr == r and nc == c - 1:
+            self.east_wall[r][c - 1] = False
+            
+        # up
+        elif nr == r - 1 and nc == c:
+            self.north_wall[r][c] = False
+            
+        # down
+        elif nr == r + 1 and nc == c:
+            self.north_wall[r + 1][c] = False
+            
+    def get_reachable_neighbors(self, r, c):
+        neighbors = []
+        
+        # right
+        if c < self.cols - 1:
+            if not self.east_wall[r][c]:
+                neighbors.append((r, c + 1))
+                
+        # left
+        if c > 0:
+            if not self.east_wall[r][c - 1]:
+                neighbors.append((r, c - 1))
+                
+        # up
+        if r > 0:
+            if not self.north_wall[r][c]:
+                neighbors.append((r - 1, c))
+        
+        # down
+        if r < self.rows - 1:
+            if not self.north_wall[r + 1][c]:
+                neighbors.append((r + 1, c))
+                
+        return neighbors
+    
+    def add_random_cycles(self, chance=20):
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if random.randint(1, chance) == 1:
+                    directions = []
+
+                    if c < self.cols - 1:
+                        directions.append("RIGHT")
+                    
+                    if r < self.rows - 1:
+                        directions.append("DOWN")
+                        
+                    if directions:
+                        d = random.choice(directions)
+                        
+                        if d == "RIGHT":
+                            self.east_wall[r][c] = False
+                        elif d == "DOWN":
+                            self.north_wall[r + 1][c] = False
+                            
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
